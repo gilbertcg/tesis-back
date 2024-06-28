b5da1abd7ea7f2cc97636c5fac33c5d1c05c1631
gchavezk <gilbert.chavez@kenility.com>
2024-06-28 15:47:23 -0400

    example

--- controllers/client.js
+++ controllers/client.js
@@ -181,7 +181,7 @@ const processAudio = async (req, res) => {
   try {
     const filePath = '/tmp/sample.wav';
     await fs.promises.writeFile(filePath, req.file.buffer);
-    const transcription = await googleSpeechController.transcribeAudio('./voip.wav');
+    const transcription = await googleSpeechController.transcribeAudio('voip.wav');
     console.log(transcription);
     console.log(transcription[0].results.map(r => r.alternatives[0].transcript).join('\n'));
     const prompt = generatePrompt('hola nos reunimos manana?', null, req.body.sentiment);
